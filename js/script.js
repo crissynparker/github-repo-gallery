b5d3600770c521a2282c47352bca2f6fd97160b6
crissynparker1 <crissynparker@gmail.com>
2021-05-12 19:32:38 -0700

    modified js/script.js

--- js/script.js
+++ js/script.js
@@ -17,10 +17,10 @@ div.innerHTML = '
 <img alt="user avatar" src = ${data.avatar_url} />
 </figure>
 <div>
-<p><strong>Name:</strong> ${}</p>
-<p><strong>Bio:</strong> ${}</p>
-<p><strong>Location:</strong> ${}</p>
-<p><strong>Number of public repos:</strong> ${}</p>
+<p><strong>Name:</strong> ${data.name}</p>
+<p><strong>Bio:</strong> ${data.bio}</p>
+<p><strong>Location:</strong> ${data.location}</p>
+<p><strong>Number of public repos:</strong> ${data.public_repos}</p>
 </div> 
 ';
 overview.append(div);
