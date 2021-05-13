b7974f1ad55576470bade0c107116649b2c585d3
crissynparker1 <crissynparker@gmail.com>
2021-05-12 19:25:33 -0700

    modified js/script.js

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const overview = document.querySelector(".overview");
+const username = "crissynparker";
+
+const gitUserInfo = async function () {
+    const userInfo = await fetch ('https://api.github.com/users/${username}');
+    const data = await userInfo.json();
+    displayUserInfo(data);
+};
+
+gitUserInfo();
+
+const displayUserInfo =  function (data) {
+const div = document.createElement("div");
+div.classList.add("user-info");
+div.innerHTML = '
+<figure>
+<img alt="user avatar" src = ${data.avatar_url} />
+</figure>
+<div>
+<p><strong>Name:</strong> ${}</p>
+<p><strong>Bio:</strong> ${}</p>
+<p><strong>Location:</strong> ${}</p>
+<p><strong>Number of public repos:</strong> ${}</p>
+</div> 
+';
+overview.append(div);
+};
+
+
